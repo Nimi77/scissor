935d52810cd886a7af2172d30fe282be357adb1a
Nimi77 <abimbolaoladejo4@gmail.com>
2024-07-09 23:44:40 +0100

    working on landingpage

---
+++ app/LandingPage/index.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "./Components/HeroSection";
+import NavBar from "./Components/NavBar";
+
+export default function LandingPage() {
+  return (
+    <>
+     <NavBar/>
+     <HeroSection/>
+    </>
+  );
+}
