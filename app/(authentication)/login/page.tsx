b6f9cf27f76f1ea48614efd699f683eae9ce462a
Nimi77 <abimbolaoladejo4@gmail.com>
2024-07-27 00:32:13 +0100

    valid route for authOptions

--- app/(authentication)/login/page.tsx
+++ app/(authentication)/login/page.tsx
@@ -1,6 +1,6 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
+// import { authOptions } from "@/lib/auth";
+// import { getServerSession } from "next-auth";
+// import { redirect } from "next/navigation";
 import LoginForm from "./form";
 
 export default async function Login() {
