1e29b2ddd99ed6bfe8318fed00eb8d9292cbd80d
Nimi77 <abimbolaoladejo4@gmail.com>
2024-09-02 13:45:36 +0100

    updated jest config.

--- jest.config.ts
+++ jest.config.ts
@@ -5,8 +5,8 @@
 
 import type {Config} from 'jest';
 
-const config: Config = { 
-
+const config: Config = {
+  
   // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
 
@@ -42,16 +42,13 @@ const config: Config = {
     "node"
   ],
 
-  // The root directory that Jest should scan for tests and modules within
-  // rootDir: undefined,
-
   // A list of paths to directories that Jest should use to search for files in
   roots: [
     "<rootDir>"
   ],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], 
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 
   preset: 'ts-jest',
 
@@ -83,6 +80,7 @@ const config: Config = {
 
   // Indicates whether each individual test should be reported during the run
   verbose: true,
+
 };
 
 export default config;
